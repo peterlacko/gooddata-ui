4f3367cd4a1a79385551b29c0f62d18b5df096c9
Nikola Čech <nikola.cech@gooddata.com>
2018-12-12 17:04:06 +0100

    FEATURE: WEB-438 Create Footer to GoodData.UI SDK Doc

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -8,22 +8,21 @@
 const React = require('react');
 
 class Footer extends React.Component {
-  docUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
-  }
-
-  pageUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? language + '/' : '') + doc;
-  }
-
   render() {
     const currentYear = new Date().getFullYear();
     return (
       <footer className="nav-footer" id="footer">
+        <section className="footer-links">
+          <h3>Find more about GoodData.UI&nbsp;on</h3>
+          <div className="social-buttons">
+            <a href="https://github.com/gooddata/gooddata-react-components" target="_blank" className="social-button github">Github</a>
+            <a href="https://stackoverflow.com/questions/tagged/gooddata" target="_blank" className="social-button stackoverflow">Stack Overflow</a>
+            <a href="https://twitter.com/gooddata_dev" target="_blank" className="social-button twitter">Twitter</a>
+            <a href="https://www.npmjs.com/package/@gooddata/react-components" target="_blank" className="social-button npm">NPM</a>
+          </div>
+        </section>
         <section className="copyright wrapper">
-          Copyright &copy; 2007–{currentYear} GoodData Corporation. All Rights Reserved. 
+          Copyright &copy; 2007–{currentYear} GoodData Corporation. All Rights Reserved.
           Code licensed under a dual license - <a href="https://github.com/gooddata/gooddata-react-components/blob/master/LICENSE">CC BY-NC 4.0 for trial experience and GoodData.UI EULA for commercial use.</a>.
         </section>
       </footer>
